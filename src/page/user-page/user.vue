547d5c52945d2ff1e5afe9cba191b7c6a90d5abe
TuLing <xiaotuling2021@gmail.com>
2022-09-06 08:24:47 +0800

    第五次提交

--- src/page/user-page/user.vue
+++ src/page/user-page/user.vue
@@ -3,9 +3,24 @@
         <div class="heading">用户列表</div>
         <div>
           <el-table :data="res" style="width: 100%">
-            <el-table-column prop="date" label="Date" width="180" />
-            <el-table-column prop="name" label="Name" width="180" />
-            <el-table-column prop="address" label="Address" />
+            <el-table-column prop="time" label="注册时间" align="center" min-width="100" />
+            <el-table-column prop="name" label="姓名" align="center" min-width="100" />
+            <el-table-column label="头像" align="center" min-width="100">
+                <!-- 头像 -->       
+                <template #default="scope">
+                        <el-image style="width: 40px; height: 40px;" 
+                              :src="scope.row.head" 
+                              fit="cover" 
+                              :preview-src-list="[scope.row.head]" 
+                              :hide-on-click-modal ="true"
+                              lazy
+                              preview-teleported="true"
+                              >
+                        </el-image>
+                </template>    
+            </el-table-column>
+            <el-table-column prop="sex" label="性别" align="center" min-width="100" />
+            <el-table-column prop="position" label="职业" align="center" min-width="100" />
           </el-table>
         </div>
     </div>
